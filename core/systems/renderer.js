21cf51406801c1d05e0b31cc42f8bf7569c730ea
will <will3.git@gmail.com>
2015-09-20 19:45:45 +1200

    Revert "Remove edges"
    This reverts commit 2ed412db08e2461106494bd139b318b7e8aef2bf.

--- core/systems/renderer.js
+++ core/systems/renderer.js
@@ -19,8 +19,10 @@ var Renderer = function(container) {
 
     //object look up, by component id
     this.objectMap = {};
+    this.edgeObjectMap = {};
 
     this.scene = new THREE.Scene();
+    this.edgeScene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     this.camera.rotation.order = 'YXZ';
@@ -35,6 +37,8 @@ var Renderer = function(container) {
 
     this.renderer = renderer;
 
+    //composers
+    this.edgeComposer = null;
     this.diffuseComposer = null;
 
     this.depthMaterial = null;
@@ -43,6 +47,7 @@ var Renderer = function(container) {
 
     this.onlyAO = false;
     this.ssao = true;
+    this.edges = true;
     this.postprocessingNeedsUpdate = false;
 
     //set up resize
@@ -67,6 +72,25 @@ Renderer.prototype.onWindowResize = function() {
 };
 
 Renderer.prototype.initPostprocessing = function() {
+    //edge composer
+    this.edgeComposer = new THREE.EffectComposer(this.renderer);
+
+    //render pass
+    var renderPass = new THREE.RenderPass(this.edgeScene, this.camera);
+    this.edgeComposer.addPass(renderPass);
+
+    //edge using canny edge filter
+    var cannyEdge = new THREE.ShaderPass(THREE.CannyEdgeFilterPass);
+    this.edgeComposer.addPass(cannyEdge);
+
+    //invert edge
+    var invert = new THREE.ShaderPass(THREE.InvertThreshholdPass);
+    this.edgeComposer.addPass(invert);
+
+    //copy edges
+    var copyPass = new THREE.ShaderPass(THREE.CopyShader);
+    this.edgeComposer.addPass(copyPass);
+
     // Setup depth pass
     var depthShader = THREE.ShaderLib["depthRGBA"];
     var depthUniforms = THREE.UniformsUtils.clone(depthShader.uniforms);
@@ -105,6 +129,13 @@ Renderer.prototype.initPostprocessing = function() {
         this.diffuseComposer.addPass(ssaoPass);
     }
 
+    if (this.edges) {
+        //blend with edge composer
+        var multiplyPass = new THREE.ShaderPass(THREE.MultiplyBlendShader);
+        multiplyPass.uniforms["tEdge"].value = this.edgeComposer.renderTarget2;
+        this.diffuseComposer.addPass(multiplyPass);
+    }
+
     //copy to scene
     var copyPass = new THREE.ShaderPass(THREE.CopyShader);
     copyPass.renderToScreen = true;
@@ -126,6 +157,10 @@ Renderer.prototype.render = function() {
         this.postprocessingNeedsUpdate = false;
     }
 
+    if (this.edges) {
+        this.edgeComposer.render();
+    }
+
     if (this.ssao) {
         // Render depth into depthRenderTarget
         this.scene.overrideMaterial = this.depthMaterial;
@@ -142,8 +177,12 @@ Renderer.prototype.tick = function() {
         var renderComponent = this.componentMap[id];
 
         if (this.objectNeedsUpdate(renderComponent)) {
-            this.removeObject(renderComponent);
-            this.addObject(renderComponent);
+            if (this.objectMap[id] != null) {
+                this.removeObject(renderComponent);
+            }
+            if (renderComponent.object != null) {
+                this.addObject(renderComponent);
+            }
         }
 
         if (renderComponent.object == null) {
@@ -163,20 +202,26 @@ Renderer.prototype.objectNeedsUpdate = function(renderComponent) {
 };
 
 Renderer.prototype.addObject = function(renderComponent) {
-    if (renderComponent.object == null) {
-        return;
-    }
-
     var object = renderComponent.object;
     this.scene.add(object);
     this.objectMap[renderComponent.id] = object;
+
+    if (renderComponent.hasEdges) {
+        var edgeObject = object.clone();
+        this.edgeObjectMap[renderComponent.id] = edgeObject;
+        this.edgeScene.add(edgeObject);
+    }
 };
 
 Renderer.prototype.removeObject = function(renderComponent) {
     var object = this.objectMap[renderComponent.id];
-    if (object != null) {
-        this.scene.remove(object);
-        delete this.objectMap[renderComponent.id];
+    this.scene.add(object);
+    delete this.objectMap[renderComponent.id];
+
+    var edgeObject = this.edgeObjectMap[renderComponent.id];
+    if (edgeObject != null) {
+        this.edgeScene.remove(edgeObject);
+        delete this.edgeObjectMap[renderComponent.id];
     }
 };
 
